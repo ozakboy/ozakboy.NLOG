718853c34d64d081b1c80c0c13688c6fd3e04c03
ozakboy <ozakboy@gmail.com>
2022-03-27 14:12:17 +0800

    新增 LOG 測試專案

---
+++ ozakboy.NLOG/ozakboy.NLOG.Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using ozakboy.NLOG;
+Console.WriteLine("Hello, World!");
+LOG.SetLogKeepDay(-7);
+LOG.Trace_Log("LOG_TEST");
+LOG.Info_Log("LOG_TEST");
+LOG.Debug_Log("LOG_TEST");
+LOG.Error_Log("LOG_TEST");
+LOG.Warn_Log("LOG_TEST");
+LOG.Fatal_Log("LOG_TEST");
+LOG.CostomName_Log("ABC","LOG_TEST");
+
+try
+{
+    throw new ErrorMessageException("發生錯誤");
+}
+catch (ErrorMessageException ex)
+{
+    LOG.Warn_Log(ex);
+}
+
+try
+{
+    throw new ErrorMessageException("發生錯誤");
+}
+catch (Exception ex)
+{
+    LOG.Warn_Log(ex);
+}
+
+try
+{
+    throw new Exception("發生錯誤");
+}
+catch (Exception ex)
+{
+    LOG.Warn_Log(ex);
+}
